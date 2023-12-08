11e33d1f52feeb37c495c1c4afba634750bc9b0f
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-12-08 18:32:20 +0200

    done with binding customAuthenticationProvider with AuthenticationManagerBuilder

--- Oauth-authorization-server/src/main/java/com/alliance/oauthserver/config/DefaultSecurityConfig.java
+++ Oauth-authorization-server/src/main/java/com/alliance/oauthserver/config/DefaultSecurityConfig.java
@@ -1,7 +1,10 @@
 package com.alliance.oauthserver.config;
 
+import com.alliance.oauthserver.service.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,10 +12,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class DefaultSecurityConfig {
 
+  @Autowired
+  private CustomAuthenticationProvider customAuthenticationProvider;
+
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
       .formLogin(Customizer.withDefaults());
     return http.build();
+  }
+
+  public void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder){
+    authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider);
+
   }
 }
